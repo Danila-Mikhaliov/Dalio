f579ead2a3344f178a558d44828d28b4475a4bd7
danila_mikhaliov <danikmikhaliov200206@gmail.com>
2022-12-10 01:01:57 +0300

    change desktop version, fixed blocks

---
+++ dist/index.6a31943b.js
@@ -0,0 +1,14 @@
+new Swiper(".swiper", {
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
+    spaceBetween: 30,
+    slidesPerView: 1.5
+});
+
+//# sourceMappingURL=index.6a31943b.js.map
